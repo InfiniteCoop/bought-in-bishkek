9bc500b045f5ade33d47fea26ca21ea35ff22d41
Cooper <Cooper@Coopers-MacBook-Pro.local>
2015-02-15 18:20:37 +0600

    style changes, mostly

--- js/grayscale.js
+++ js/grayscale.js
@@ -24,6 +24,8 @@ $(function() {
     });
 });
 
+var skrollzorz = skrollr.init();
+
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
